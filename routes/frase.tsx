961d732889eeef1ca3a7abadf7fc46ac8b8b2d73
Daniel <dcastanedar@alumnos.nebrija.es>
2024-03-03 19:04:03 +0100

    upload

--- routes/frase.tsx
+++ routes/frase.tsx
@@ -15,6 +15,14 @@ export const handler: Handlers = {
 const Page = (props: PageProps<Frase>) => {
     return (
         <body class={"img-fondo1"}>
+            <div class="capas">
+              <img src="https://i.discogs.com/XCXiemMRkFL2oLIAXcs8cqg8SBH-qevIrflVPm5N7KQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY4MDg5/MDctMTQ2NjcyNTA0/My02MDE2LmpwZWc.jpeg" class="capa0"/>
+              <img src="https://i.discogs.com/XCXiemMRkFL2oLIAXcs8cqg8SBH-qevIrflVPm5N7KQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY4MDg5/MDctMTQ2NjcyNTA0/My02MDE2LmpwZWc.jpeg" class="capa1"/>
+              <img src="https://i.discogs.com/XCXiemMRkFL2oLIAXcs8cqg8SBH-qevIrflVPm5N7KQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY4MDg5/MDctMTQ2NjcyNTA0/My02MDE2LmpwZWc.jpeg" class="capa2"/>
+              <img src="https://i.discogs.com/XCXiemMRkFL2oLIAXcs8cqg8SBH-qevIrflVPm5N7KQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY4MDg5/MDctMTQ2NjcyNTA0/My02MDE2LmpwZWc.jpeg" class="capa3"/>
+              <img src="https://i.discogs.com/XCXiemMRkFL2oLIAXcs8cqg8SBH-qevIrflVPm5N7KQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY4MDg5/MDctMTQ2NjcyNTA0/My02MDE2LmpwZWc.jpeg" class="capa4"/>
+              <a href={"https://youtu.be/xm3YgoEiEDc?si=iu97snIom81RzvT_"}><img src="https://i.discogs.com/XCXiemMRkFL2oLIAXcs8cqg8SBH-qevIrflVPm5N7KQ/rs:fit/g:sm/q:90/h:600/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTY4MDg5/MDctMTQ2NjcyNTA0/My02MDE2LmpwZWc.jpeg" class="capa5"/></a>
+            </div>
             <div>
               <p class="texto-api">{props.data}</p>
               <a href={"/frase"}><button class="start-boton"></button></a>
